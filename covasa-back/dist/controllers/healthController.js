778a9aeca99ddb6b51d09d7ce11fc97c9f045f75
RIDSdiseno <diseno@rids.cl>
2025-12-29 17:29:13 -0300

    ola

--- covasa-back/dist/controllers/healthController.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.healthCheck = void 0;
-const healthCheck = (_req, res) => {
-    res.json({ status: "ok" });
-};
-exports.healthCheck = healthCheck;
